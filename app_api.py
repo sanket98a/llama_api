4e3c297a739705512976f4c9134344f183ee3e07
sanket98a <bodake871@gmail.com>
2023-08-29 09:18:53 +0530

    file add

---
+++ app_api.py
@@ -0,0 +1,47 @@
+from langchain.llms import HuggingFacePipeline, LlamaCpp,CTransformers
+from model_laoding import ModelClass
+import streamlit as st
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+with st.sidebar:
+    model_name=st.selectbox("Select Model :-",['Llama-2-7B-Chat','CodeLlama-7B-Instruct'])
+    temperature=st.slider("Temperature :-",0.0,1.0,0.1)
+    top_p=st.slider("top_p :-",0.0,1.0,0.95)
+    top_k=st.slider("top_k :- ",0,100,50)
+    # DEFAULT_SYSTEM_PROMPT=st.text_area("System Prompt :-",f"{DEFAULT_SYSTEM_PROMPT}",height=400)
+
+# Load the selected model
+if model_name=="Llama-2-7B-Chat":
+    print("Llama 7B model Loading")
+    model_id="TheBloke/Llama-2-7B-Chat-GGML"
+    model_basename="llama-2-7b-chat.ggmlv3.q4_0.bin"
+else:
+    print("CodeLlama-7B-Instruct-GGML model Loading")
+    model_id="TheBloke/CodeLlama-7B-Instruct-GGML"
+    model_basename="codellama-7b-instruct.ggmlv3.Q2_K.bin"
+
+## Load the Local Llama 2 model
+
+model=ModelClass(model_id,model_basename)
+def generate_text(prompt, max_length=50,temp=0.7): 
+    param={"temperature":temp,
+            "max_tokens":max_length}
+    return model.generate(prompt,params=param)
+
+@app.route("/v1/generate_text/", methods=["POST"])
+def generate_text_api():
+    data = request.get_json()
+    prompts = data.get("prompts")
+    max_length = data.get("max_length", 50)
+    temp=data.get("temperature",0.7)
+    # print(prompts)
+    if not prompts:
+        return jsonify({"error": "Invalid input. 'prompts' field must be a non-empty list."}), 400
+
+    generated_texts = [generate_text(prompt, max_length,temp) for prompt in prompts]
+    return jsonify({"generated_texts": generated_texts})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000,debug=True)
---
+++ model_laoding.py
@@ -0,0 +1,60 @@
+from langchain.llms import HuggingFacePipeline, LlamaCpp,CTransformers
+import torch
+from huggingface_hub import hf_hub_download
+
+if torch.cuda.is_available():
+    device_type = "cuda:0"
+else:
+    device_type = "cpu"
+
+class ModelClass:
+    
+    def __init__(self,model_id,model_basename,temperature):
+        model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
+  
+        kwargs = {
+            "model_path": model_path,
+            "n_ctx": 2014,
+            "max_tokens": 2014,
+            }
+        if device_type.lower() == "mps":
+            kwargs["n_gpu_layers"] = 20
+        if device_type.lower() == "cuda:0":
+            kwargs["n_gpu_layers"] = 15
+            kwargs["n_batch"] = 40
+            kwargs["temperature"] =temperature
+        print("GGML Model Loaded Succesfully.")
+        self.model=LlamaCpp(**kwargs)
+            
+
+    def generate(self, prompt: str, params: dict[str, str])-> str:
+        if params is None:
+            params = {}
+        suffix = params.get("suffix")
+        max_tokens = params.get("max_tokens", 128)
+        temperature = params.get("temperature", 0.8)
+        top_p = params.get("top_p", 0.95)
+        logprobs = params.get("logprobs")
+        echo = params.get("echo", False)
+        stop: any = params.get("stop", [])
+        frequency_penalty = params.get("frequency_penalty", 0.0)
+        presence_penalty = params.get("presence_penalty", 0.0)
+        repeat_penalty = params.get("repeat_penalty", 1.1)
+        top_k = params.get("top_k", 40)
+
+        result = self.model(
+            prompt=prompt,
+            suffix=suffix,
+            max_tokens=max_tokens,
+            temperature=temperature,
+            top_p=top_p,
+            logprobs=logprobs,
+            echo=echo,
+            stop=stop,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            repeat_penalty=repeat_penalty,
+            top_k=top_k,
+        )
+        return result
+                    
